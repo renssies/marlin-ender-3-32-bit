21b1faf5d64c628a486131492ded5f0fc82d4ada
Rens Verhoeven <rens@renssies.nl>
2019-10-19 17:46:48 +0200

    Adjust preheat temps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1439,8 +1439,8 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_HOTEND 195
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
